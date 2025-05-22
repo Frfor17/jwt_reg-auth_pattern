184ed029548602dbaba6b67f846276d640c09944
Фёдор Хлевицкий <brother.23@yandex.ru>
2025-05-22 21:28:05 +0300

    ee
    ee

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import asyncio
+from sqlalchemy.future import select
+from database import SessionLocal, engine
+from models import User, Base
 from fastapi import FastAPI
 
-app = FastAPI
+app = FastAPI()
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
