f2f4d792276a3e8368a48a97cd87c6b523147f97
Фёдор Хлевицкий <brother.23@yandex.ru>
2025-05-24 19:08:52 +0300

    add Get All and POST methods
    давал гет алл юзерс и пост алл юзерс

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from sqlalchemy.future import select
 from database import SessionLocal, engine
 from models import User, Base
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import AsyncSession
 
 app = FastAPI()
 
-
-async def init_db():
-    async with engine.begin() as conn:
+# это код инициализирует базу данных 
+async def init_db():  # создаётся ф-ия для инициализации БД
+    async with engine.begin() as conn:  # открывается контекст менеджер, создаётся транзакция и подключение к БД
         await conn.run_sync(Base.metadata.create_all)
+
+# создание юзера
+async def create_user(db: AsyncSession, user_id: int)
+    new_user = User(name=name, password=password)
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    return new_user
+
+# чтение юзера 
+async def get_all_users(db: AsyncSession)
+    result = await db.execute(select(User))
+    return result.scalars().all()
