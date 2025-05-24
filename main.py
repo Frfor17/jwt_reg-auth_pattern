82b03b3248ef01c20f051e64e642152779dee9b3
Фёдор Хлевицкий <brother.23@yandex.ru>
2025-05-24 19:30:30 +0300

    added schemas and links to them at endpoints
    добавил pydantic схемы и ссылки на них в ручках

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+#внешние импорты
 import asyncio
 from sqlalchemy.future import select
+from fastapi import FastAPI, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Any, AsyncGenerator
+
+# Импорты из локальных файлов
+from schemas import UserCreate, UserOut
 from database import SessionLocal, engine
 from models import User, Base
-from fastapi import FastAPI
-from sqlalchemy.ext.asyncio import AsyncSession
 
 app = FastAPI()
 
@@ -12,8 +17,14 @@ async def init_db():  # создаётся ф-ия для инициализац
     async with engine.begin() as conn:  # открывается контекст менеджер, создаётся транзакция и подключение к БД
         await conn.run_sync(Base.metadata.create_all)
 
+#какая-то странная функция
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
+
 # создание юзера
-async def create_user(db: AsyncSession, user_id: int)
+@app.post("/users/", response_model=UserCreate)
+async def create_user(name: str, password: str, AsyncSession = Depends(get_db)) -> Any:
     new_user = User(name=name, password=password)
     db.add(new_user)
     await db.commit()
@@ -21,6 +32,7 @@ async def create_user(db: AsyncSession, user_id: int)
     return new_user
 
 # чтение юзера 
-async def get_all_users(db: AsyncSession)
+@app.get("/users/", response_model=UserOut)
+async def get_all_users(db: AsyncSession):
     result = await db.execute(select(User))
     return result.scalars().all()
