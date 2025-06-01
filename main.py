c0bfaa06f073bf2c65ab3c19e0a9c2aa2978abc1
Фёдор Хлевицкий <brother.23@yandex.ru>
2025-06-01 10:11:39 +0300

    добавил вход по JWT токену

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #внешние импорты
 import asyncio
 from sqlalchemy.future import select
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Any, AsyncGenerator, List
-from passlib.context import CryptContext 
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm 
+
 
 # Импорты из локальных файлов
 from schemas import UserCreate, UserOut
@@ -23,10 +27,28 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         yield session
 
+SECRET_KEY = "your-secret-key"  # замени на более надёжный и храни в .env
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_user_by_name(db: AsyncSession, name: str) -> User | None:
+    result = await db.execute(select(User).where(User.name == name))
+    return result.scalar_one_or_none() 
 
 # создание юзера
 @app.post("/users/", response_model=UserCreate)
@@ -43,3 +65,16 @@ async def create_user(name: str, password: str, db: AsyncSession = Depends(get_d
 async def get_all_users(db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(User))
     return result.scalars().all()
+
+# вход, логин юзера
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    user = await get_user_by_name(db, form_data_data.username)
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверное имя пользователя или пароль",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub":user.name}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    return {"access_token": access_token, "token_type": "bearer"}
